3f18f13e7132c2641dc3488943d7ddacbe79714e
Kashyap Gohil <kashyapgohil476@gmail.com>
2025-02-02 06:07:16 +0530

    [utility] updates cx definition to include undefined and null checks

--- ui/web/src/utility/cx.ts
+++ ui/web/src/utility/cx.ts
@@ -1,16 +1,16 @@
-export function cx(...classnames: Array<string | Record<string, boolean>>) {
-  const result: Array<string> = [];
-  classnames.forEach((classnames) => {
-    if (typeof classnames === "string") {
-      result.push(classnames);
-    }
-    if (typeof classnames === "object") {
-      result.push(
-        Object.keys(classnames)
-          .filter((key) => classnames[key])
-          .join(" "),
-      );
-    }
-  });
-  return result.join(" ");
+export function cx(...classnames: Array<string | Record<string, boolean> | undefined | null>) {
+	const result: Array<string> = [];
+	classnames.forEach((classname) => {
+		if (typeof classname === 'string') {
+			result.push(classname);
+		}
+		if (typeof classname === 'object' && classname) {
+			result.push(
+				Object.keys(classname)
+					.filter((key) => classname[key])
+					.join(' ')
+			);
+		}
+	});
+	return result.join(' ');
 }
