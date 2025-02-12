7768fae7e3d5e3c13af015debd223d51a3b2ff94
Kashyap Gohil <kashyapgohil476@gmail.com>
2025-02-12 22:36:20 +0530

    [utility] adds mapObj to map utility

--- ui/web/src/utility/map.ts
+++ ui/web/src/utility/map.ts
@@ -3,10 +3,25 @@
  * @param fn function to loop through list
  * @returns list of modified items (returned by fn)
  */
-export function map<T, R>(list: Array<T>, fn: (item: T, index: number) => R) {
+export function map<T, R>(
+  list: Array<T>,
+  fn: (item: T, index: number) => R,
+): Array<R> {
   const ans: Array<R> = [];
   for (let index = 0; index < list.length; index++) {
     const value = fn(list[index], index);
+    ans.push(value);
+  }
+  return ans;
+}
+
+export function mapObj<T, R>(
+  object: T,
+  fn: (value: T[keyof T], key: keyof T) => R,
+): Array<R> {
+  const ans: Array<R> = [];
+  for (const key in object) {
+    const value = fn(object[key], key);
     ans.push(value);
   }
   return ans;
