24c5e4ae225471a736917a8e9f83d6387a77844d
Kashyap Gohil <kashyapgohil476@gmail.com>
2025-02-12 22:23:05 +0530

    [utility] adds tailwind merge into cx

--- ui/web/src/utility/cx.ts
+++ ui/web/src/utility/cx.ts
@@ -1,16 +1,20 @@
-export function cx(...classnames: Array<string | Record<string, boolean> | undefined | null>) {
-	const result: Array<string> = [];
-	classnames.forEach((classname) => {
-		if (typeof classname === 'string') {
-			result.push(classname);
-		}
-		if (typeof classname === 'object' && classname) {
-			result.push(
-				Object.keys(classname)
-					.filter((key) => classname[key])
-					.join(' ')
-			);
-		}
-	});
-	return result.join(' ');
+import { twMerge } from "tailwind-merge";
+
+export function cx(
+  ...classnames: Array<string | Record<string, boolean> | undefined | null>
+) {
+  const result: Array<string> = [];
+  classnames.forEach((classname) => {
+    if (typeof classname === "string") {
+      result.push(classname);
+    }
+    if (typeof classname === "object" && classname) {
+      result.push(
+        Object.keys(classname)
+          .filter((key) => classname[key])
+          .join(" "),
+      );
+    }
+  });
+  return twMerge(result);
 }
