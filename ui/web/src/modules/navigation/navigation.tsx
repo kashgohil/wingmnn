3b5ba6481010d58e41b5a964ec66fd837a8ec97c
Kashyap Gohil <kashyapgohil476@gmail.com>
2025-02-15 00:03:03 +0530

    [chore] utility change

--- ui/web/src/modules/navigation/navigation.tsx
+++ ui/web/src/modules/navigation/navigation.tsx
@@ -1,9 +1,10 @@
+import { mapObj } from "@utility/map";
 import { ModulesConfig } from "./config";
 
 export function Navigation() {
   return (
     <div className="flex flex-col p-2 bg-zinc-600 h-full space-y-2">
-      {ModulesConfig.map((config) => (
+      {mapObj(ModulesConfig, (config) => (
         <div>{config.label}</div>
       ))}
     </div>
