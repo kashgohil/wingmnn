1daf3b4a44dccf90541e3169650fc7157279303e
Kashyap Gohil <kashyapgohil476@gmail.com>
2025-03-01 23:52:44 +0530

    [utility] adds reduce utility

---
+++ ui/web/src/utility/reduce.ts
@@ -0,0 +1,28 @@
+/**
+ * @param array list of items
+ * @param reducer function to loop through list
+ * @returns reduced value
+ */
+export function reduce<T, U>(
+  array: Array<T>,
+  reducer: (acc: U, item: T, index: number) => U,
+  initialValue: U,
+): U {
+  let result = initialValue;
+  for (let index = 0; index < array.length; index++) {
+    result = reducer(result, array[index], index);
+  }
+  return result;
+}
+
+export function reduceObj<T, U>(
+  object: T,
+  reducer: (acc: U, item: T[keyof T], key: keyof T) => U,
+  initialValue: U,
+): U {
+  let result = initialValue;
+  for (const key in object) {
+    result = reducer(result, object[key], key);
+  }
+  return result;
+}
