fc37ce3283af7ce4f9e275b4357381ef9835c349
Kashyap Gohil <kashyapgohil476@gmail.com>
2025-03-09 23:38:45 +0530

    [utility][cookie] added cookie setter and csrf cookie setter

--- ui/web/src/utility/browser.ts
+++ ui/web/src/utility/browser.ts
@@ -1,3 +1,5 @@
+import { uuid } from "./uuid";
+
 export const Cookie = (function () {
   function get(name: string) {
     return document.cookie.split(";").reduce((acc, item) => {
@@ -5,5 +7,35 @@ export const Cookie = (function () {
       return key.trim() === name ? decodeURIComponent(value) : acc;
     }, "");
   }
-  return { get };
+
+  function remove(name: string) {
+    document.cookie = `${name}=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;`;
+  }
+
+  function set(
+    name: string,
+    value: string,
+    options: { expires?: Date; path?: string } = {
+      expires: undefined,
+      path: "/",
+    },
+  ) {
+    let cookie = `${name}=${encodeURIComponent(value)}`;
+    if (options.expires) {
+      cookie += `; expires=${options.expires.toUTCString()}`;
+    }
+    if (options.path) {
+      cookie += `; path=${options.path}`;
+    }
+    document.cookie = cookie;
+  }
+
+  function setCSRFCookie() {
+    Cookie.set("csrf_token", uuid(), {
+      expires: undefined,
+      path: "/",
+    });
+  }
+
+  return { get, remove, set, setCSRFCookie };
 })();
