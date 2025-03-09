c727e983ac46c11361c60ff2fdae61fe2ba47782
Kashyap Gohil <kashyapgohil476@gmail.com>
2025-03-09 23:19:30 +0530

    [router] removed tanstack router setup

--- ui/web/src/routes/routeTree.ts
+++
@@ -1,5 +0,0 @@
-import { rootRoute } from "./rootRoute";
-import { HomeRoute } from "@home/route";
-import { TestingRoute } from "@testing/route";
-
-export const routeTree = rootRoute.addChildren([HomeRoute, TestingRoute]);
