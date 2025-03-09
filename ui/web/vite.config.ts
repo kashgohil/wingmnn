fc1abd148944627ab14ff7ff0a6f71e6bb12da84
Kashyap Gohil <kashyapgohil476@gmail.com>
2025-03-09 23:20:21 +0530

    [setup] added api proxy and removed tanstack rounter plugin from vite config

--- ui/web/vite.config.ts
+++ ui/web/vite.config.ts
@@ -1,21 +1,27 @@
-import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
-import tsConfigPaths from "vite-tsconfig-paths";
-import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 import viteReact from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import tsConfigPaths from "vite-tsconfig-paths";
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    proxy: {
+      "/api": {
+        secure: false,
+        changeOrigin: true,
+        target: "http://localhost:3000",
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    },
+  },
   plugins: [
-        viteReact({
-            babel: {
-                plugins: [
-                    ["babel-plugin-react-compiler"]
-                ]
-            }
-        }),
+    viteReact({
+      babel: {
+        plugins: [["babel-plugin-react-compiler"]],
+      },
+    }),
     tailwindcss(),
     tsConfigPaths(),
-    TanStackRouterVite(),
   ],
 });
