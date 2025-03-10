c99759de3d642e04f18d04f8e4af3cc6703dba41
Kashyap Gohil <kashyapgohil476@gmail.com>
2025-03-10 16:31:19 +0530

    [router] added home link in not found view

--- ui/web/src/routes/authRouter.tsx
+++ ui/web/src/routes/authRouter.tsx
@@ -1,8 +1,11 @@
 import { Button } from "@components/button/button";
+import { Link } from "@frameworks/router/Link";
 import { useRouter } from "@frameworks/router/useRouter";
 import { Wingmnn } from "@icons/wingmnn";
 import { Landing } from "@landing/landing";
+import { Modules } from "@navigation/constants";
 import { Navigation } from "@navigation/navigation";
+import { BaseRoutes } from "@navigation/routes";
 import { AuthService } from "@services/authService";
 import { ROUTES_CONFIG } from "./config";
 
@@ -13,23 +16,29 @@ export function AuthRouter() {
     return <Landing />;
   }
 
-  if (!Component) {
-    return (
-      <div className="w-full h-full flex flex-col items-center justify-center">
-        <div className="text-4xl">You look lost, my friend</div>
-        <div className="text-2xl">Let's get you to where all the fun stuff is!!</div>
-        <Button className="center-y space-y-2" size="sm">
-          <Wingmnn className="animate-slow-spin" /> Take me Home, Country Roads
-        </Button>
-      </div>
-    );
+  function content() {
+    if (!Component) {
+      return (
+        <div className="w-full h-full flex flex-col items-center justify-center space-y-2">
+          <div className="text-2xl">You look lost</div>
+          <div className="text-lg">Let's get you to where all the fun stuff is!!</div>
+          <Link to={BaseRoutes[Modules.HOME]}>
+            <Button className="flex items-center space-x-3 bg-white-500 text-black-200 mt-4 hover:bg-white-950">
+              <Wingmnn height={24} width={24} className="animate-slow-spin" /> <span>Take me Home, Country Roads</span>
+            </Button>
+          </Link>
+        </div>
+      );
+    }
+
+    return <Component />;
   }
 
   return (
     <div className="w-full h-full flex p-2">
       <Navigation />
       <div className="flex-1 overflow-hidden pl-2 h-full">
-        <Component />
+        {content()}
       </div>
     </div>
   );
