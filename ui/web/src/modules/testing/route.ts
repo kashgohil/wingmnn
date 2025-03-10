2f629122bb9f07ff9a4f98cf3839fa700dd9dfd7
Kashyap Gohil <kashyapgohil476@gmail.com>
2025-03-10 17:44:27 +0530

    [testing] removed testing routes file

--- ui/web/src/modules/testing/route.ts
+++
@@ -1,7 +0,0 @@
-import { rootRoute } from "@routes/rootRoute";
-import { createRoute } from "@tanstack/react-router";
-
-export const TestingRoute = createRoute({
-  path: "/testing",
-  getParentRoute: () => rootRoute,
-}).lazy(() => import("./testing").then((r) => r.TestingRoute));
