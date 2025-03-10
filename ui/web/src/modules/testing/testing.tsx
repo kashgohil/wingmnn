3fbc4fde63d720ee9cbaaac689bc03577ac644f2
Kashyap Gohil <kashyapgohil476@gmail.com>
2025-03-10 17:44:43 +0530

    [testing] removed routes from testing component

--- ui/web/src/modules/testing/testing.tsx
+++ ui/web/src/modules/testing/testing.tsx
@@ -1,15 +1,9 @@
 import { Editor } from "@frameworks/editor/components/editor";
-import { createLazyRoute } from "@tanstack/react-router";
 
-function Testing() {
-  console.log("is this rendering?");
+export function Testing() {
   return (
     <div>
       <Editor value=":" onChange={() => {}} />
     </div>
   );
 }
-
-export const TestingRoute = createLazyRoute("/")({
-  component: Testing,
-});
