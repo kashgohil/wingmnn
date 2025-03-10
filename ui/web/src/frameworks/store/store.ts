bcda1e2d06d4c94e9f85cfbb209203e5e4334e4d
Kashyap Gohil <kashyapgohil476@gmail.com>
2025-03-10 17:13:34 +0530

    [store] added types for state key suggestions, added safe check where same changes don't trigger a re-render

--- ui/web/src/frameworks/store/store.ts
+++ ui/web/src/frameworks/store/store.ts
@@ -1,44 +1,80 @@
+import { get as _get } from "@utility/get";
+import { isEqual } from "@utility/isEqual";
 import { useSyncExternalStore } from "react";
 
-export function create<T>(
+export function create<TState, TActions>(
   creator: (
-    set: (value: Partial<T> | ((state: T) => Partial<T>)) => void,
-    get: (key: keyof T) => T[keyof T],
-  ) => T,
+    set: (
+      value: Partial<TState> | ((state: TState) => Partial<TState>),
+    ) => void,
+    get: <TKey extends keyof TState | keyof TActions>(
+      key: TKey,
+    ) => TKey extends keyof TState
+      ? TState[TKey]
+      : TKey extends keyof TActions
+        ? TActions[TKey]
+        : never,
+  ) => TState & TActions,
 ) {
-  function set(value: Partial<T> | ((state: T) => Partial<T>)) {
-    let updates = value;
+  function set(value: Partial<TState> | ((state: TState) => Partial<TState>)) {
+    let updates: TSAny = value;
     if (typeof value === "function") updates = value(_store);
-    Object.assign({}, _store, updates);
+
+    const finalUpdates: TSAny = {};
 
     for (const key in updates) {
+      if (!isEqual(_get(_store, key), updates[key])) {
+        finalUpdates[key] = updates[key];
+      }
+    }
+
+   _store = Object.assign({}, _store, finalUpdates);
+
+    for (const key in finalUpdates) {
       for (const subscriber of _subscribers[key]) {
         subscriber();
       }
     }
   }
 
-  function get(key: keyof T) {
-    return _store[key];
+  function get<TKey extends keyof TState | keyof TActions>(key: TKey) {
+    return _store[key] as TKey extends keyof TState
+      ? TState[TKey]
+      : TKey extends keyof TActions
+        ? TActions[TKey]
+        : never;
   }
 
-  const _store = creator(set, get);
+  let _store = creator(set, get);
   const _subscribers: MapOf<Set<() => void>> = {};
 
-  function subscribe(subscribe: () => void, key: keyof T) {
-    if (!_subscribers[key as string])
+  function subscribe(
+    subscribe: () => void,
+    key: keyof TState | keyof TActions,
+  ) {
+    if (typeof _store[key] === "function") return () => {};
+    if (!_subscribers[key as string]) {
       _subscribers[key as string] = new Set([subscribe]);
-    else _subscribers[key as string].add(subscribe);
+    }
+    _subscribers[key as string].add(subscribe);
 
     return () => {
       _subscribers[key as string].delete(subscribe);
     };
   }
 
-  return function useState(key: keyof T) {
-    return useSyncExternalStore(
+  return function useState<TKey extends keyof TState | keyof TActions>(
+    key: TKey,
+  ) {
+    return useSyncExternalStore<
+      TKey extends keyof TState
+        ? TState[TKey]
+        : TKey extends keyof TActions
+          ? TActions[TKey]
+          : never
+    >(
       (subscribeFn) => subscribe(subscribeFn, key),
-      () => get(key),
+      () => get(key) as TSAny,
     );
   };
 }
