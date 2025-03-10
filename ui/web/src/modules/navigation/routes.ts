a9c131e9dd947771cdd08c15a567573b46e4fe3a
Kashyap Gohil <kashyapgohil476@gmail.com>
2025-03-10 16:34:22 +0530

    [navigation] added base routes config

---
+++ ui/web/src/modules/navigation/routes.ts
@@ -0,0 +1,20 @@
+import { Modules } from "./constants";
+
+export const BaseRoutes: Record<Modules, string> = {
+  [Modules.HOME]: "/",
+  [Modules.CALENDAR]: "/calendar",
+  [Modules.FEEDS]: "/feeds",
+  [Modules.FILES]: "/files",
+  [Modules.FINANCE]: "/finance",
+  [Modules.PROJECTS]: "/projects",
+  [Modules.GAMES]: "/games",
+  [Modules.MAILS]: "/mails",
+  [Modules.MUSIC]: "/music",
+  [Modules.MESSAGES]: "/messages",
+  [Modules.NOTES]: "/notes",
+  [Modules.PROFILE]: "/profile",
+  [Modules.RESUMES]: "/resumes",
+  [Modules.WELLNESS]: "/wellness",
+  [Modules.SETTINGS]: "/settings",
+  [Modules.SHEETS]: "/sheets",
+};
