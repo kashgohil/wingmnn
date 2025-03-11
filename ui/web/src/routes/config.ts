74508c08d0e342884b10d32e8044758fdb02b35e
Kashyap Gohil <kashyapgohil476@gmail.com>
2025-03-11 11:52:38 +0530

    [router] added router config

--- ui/web/src/routes/config.ts
+++ ui/web/src/routes/config.ts
@@ -1,36 +1,8 @@
 import { RouterConfig } from "@frameworks/router/type";
-import { Modules } from "@navigation/constants";
-import { Mails } from "src/modules/mails/mails";
-import { Projects } from "src/modules/projects/projects";
+import { HomeRoute } from "@home/route";
+import { ProjectsRoute } from "@projects/route";
 
 export const ROUTES_CONFIG: RouterConfig = [
-  {
-    id: Modules.MAILS,
-    name: 'Mails',
-    description: 'Manage your emails',
-    path: '/mails',
-    Component: Mails,
-    childRoutes: [
-      {
-        id: Modules.MAILS,
-        path: '/:id',
-        Component: Mails,
-      },
-    ],
-  },
-  {
-    id: Modules.PROJECTS,
-    name: 'Projects',
-    description: 'Manage your projects',
-    path: '/projects',
-    Component: Projects,
-    childRoutes: [
-      {
-        id: Modules.PROJECTS,
-        path: '/:id',
-        Component: Projects,
-      },
-    ],
-  },
-
+  HomeRoute,
+  ProjectsRoute,
 ]
