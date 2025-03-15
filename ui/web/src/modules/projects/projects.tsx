2bc7a227a2c2f460cd74cf99d13018c33ed82f81
Kashyap Gohil <kashyapgohil476@gmail.com>
2025-03-15 17:08:46 +0530

    [projects] Add breadcrumbs and project dialogs to Projects component

--- ui/web/src/modules/projects/projects.tsx
+++ ui/web/src/modules/projects/projects.tsx
@@ -1,10 +1,27 @@
-import { Breadcrumbs } from "@components/breadcrumbs/breadcrumbs";
+import { Breadcrumb, Breadcrumbs } from "@components/breadcrumbs/breadcrumbs";
+import { Modules } from "@navigation/constants";
+import { BaseRoutes } from "@navigation/routes";
+import { ProjectDialogs } from "./components/dialogs/projectDialogs";
 
-export function Projects({ children}: { children: React.ReactNode }) {
+export function Projects({ children }: { children: React.ReactNode }) {
+  const breadcrumbs = [
+    {
+      id: "HOME",
+      name: "Home",
+      type: "link",
+      to: BaseRoutes[Modules.PROJECTS],
+    },
+  ] as Array<Breadcrumb>;
+
   return (
+    <>
       <div className="flex flex-col h-full">
-        <Breadcrumbs breadcrumbs={[]} />
+        <div className="h-14 flex items-center">
+          <Breadcrumbs breadcrumbs={breadcrumbs} />
+        </div>
         {children}
       </div>
+      <ProjectDialogs />
+    </>
   );
 }
