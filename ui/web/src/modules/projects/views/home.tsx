88e7c07134368c554e405738e39a99a6a4e90413
Kashyap Gohil <kashyapgohil476@gmail.com>
2025-03-15 17:08:25 +0530

    [projects] Add empty project state with create project button

--- ui/web/src/modules/projects/views/home.tsx
+++ ui/web/src/modules/projects/views/home.tsx
@@ -1,9 +1,28 @@
+import { Button } from "@components/button/button";
+import { Dialogs, useProjectDialog } from "@projects/logic/useProjectDialog";
+import { useProjects } from "@projects/logic/useProjects";
+import { isEmpty } from "@utility/isEmpty";
 
 export function Home() {
+  const projects = useProjects("projects");
+  const openDialog = useProjectDialog("openDialog");
 
-  return (
-    <div className="flex flex-col">
-      Projects
-    </div>
-  );
+  if (isEmpty(projects)) {
+    return (
+      <div className="h-full flex flex-col space-y-4 items-center justify-center">
+        <div className="text-center text-white-850 text-xl">
+          You don't have any projects yet.
+        </div>
+        <Button
+          size="sm"
+          variant="secondary"
+          onClick={() => openDialog(Dialogs.CREATE_PROJECT)}
+        >
+          Let's create one
+        </Button>
+      </div>
+    );
+  }
+
+  return <div className="flex flex-col"></div>;
 }
