2f061b85691bbcb7a68cd85bd40993ee3155a3d4
Kashyap Gohil <kashyapgohil476@gmail.com>
2025-03-18 23:04:08 +0530

    [projects] basic projects home setup

--- ui/web/src/modules/home/home.tsx
+++ ui/web/src/modules/home/home.tsx
@@ -1,3 +1,7 @@
 export function Home() {
-  return <div>Landing page</div>;
+  return (
+    <div className="flex flex-col h-full items-center justify-center">
+      <h1 className="text-4xl font-light">Welcome Home, Kash!</h1>
+    </div>
+  );
 }
