24c126da00973f7007baac01dd31a9ef58912f20
Kashyap Gohil <kashyapgohil476@gmail.com>
2025-04-20 06:14:59 +0530

    [utilis] update equality checks in isEqual

--- ui/web/src/utility/isEqual.ts
+++ ui/web/src/utility/isEqual.ts
@@ -2,16 +2,12 @@ import { forEachObj } from "./forEach";
 import { isPrimitive } from "./isPrimitive";
 
 /**
- *
  * @param value1 primary value
  * @param value2 secondary value
  * @returns boolean - true, if both are equal, false, if not
  */
 export function isEqual(value1: TSAny, value2: TSAny): boolean {
-  const value1Keys = Object.keys(value1);
-  const value2Keys = Object.keys(value2);
-
-  if (value1Keys.length !== value2Keys.length) return false;
+  if (Object.keys(value1).length !== Object.keys(value2).length) return false;
 
   let ans = true;
 
