6757a2775098d58925578092994ce90ad44fc763
Kashyap Gohil <kashyapgohil476@gmail.com>
2025-05-04 18:25:42 +0530

    [mails] add resizable panels to mails screen

--- ui/web/src/modules/mails/mails.tsx
+++ ui/web/src/modules/mails/mails.tsx
@@ -1,3 +1,9 @@
+import {
+  ResizableHandle,
+  ResizablePanel,
+  ResizablePanelGroup,
+} from "@components/ui/resizable";
+
 interface Props {
   children: React.ReactNode;
 }
@@ -7,8 +13,17 @@ export function Mails(props: Props) {
 
   return (
     <div className="flex h-full w-full">
-      <div className="bg-black-200 w-[15%] h-full"></div>
-      {children}
+      <ResizablePanelGroup direction="horizontal" autoSaveId={"MAIL_SPLIT"}>
+        <ResizablePanel className="bg-black-200" defaultValue={"20%"}>
+          <div className="flex flex-col p-3 h-full"></div>
+        </ResizablePanel>
+        <ResizableHandle
+          withHandle
+          handleClassName="hover:"
+          className="border-l border-l-black-100 border-r border-r-black-200 hover:border-black-200 active:border-black-200 transition-colors duration-100"
+        />
+        <ResizablePanel defaultValue={"80%"}>{children}</ResizablePanel>
+      </ResizablePanelGroup>
     </div>
   );
 }
