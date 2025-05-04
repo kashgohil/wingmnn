50d45563f5b8e905fd78fdcc57bc6f252e3c5166
Kashyap Gohil <kashyapgohil476@gmail.com>
2025-05-04 18:21:16 +0530

    [frameworks][monitoring] add error boundary component

---
+++ ui/web/src/frameworks/monitoring/components/errorBoundary.tsx
@@ -0,0 +1,61 @@
+import { Button } from "@components/button/button";
+import { playMouseClickSound } from "@utility/sounds/click";
+import React from "react";
+
+interface Props {
+  tree: string;
+}
+
+interface State {
+  hasError: boolean;
+}
+
+export class ErrorBoundary extends React.Component<
+  React.PropsWithChildren<Props>,
+  State
+> {
+  constructor(props: Props) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  static getDerivedStateFromError() {
+    return { hasError: true };
+  }
+
+  componentDidCatch(error: Error, errorInfo: React.ErrorInfo) {
+    console.error(
+      "ErrorBoundary caught an error: \n\n ERROR:\n",
+      "[" +
+        this.props.tree +
+        "]" +
+        "[" +
+        error.name +
+        "]" +
+        " " +
+        error.message,
+
+      "\n\n COMPONENT STACK:",
+      errorInfo.componentStack,
+    );
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return (
+        <div className="h-full w-full flex flex-col items-center justify-center space-y-2 bg-black-100 rounded-lg">
+          <p className="text-center text-2xl">Hmmm. Something went wrong.</p>
+          <p className="text-center">
+            looks like a bug. Hit the report button and let us know what
+            happened.
+          </p>
+          <Button onMouseDown={playMouseClickSound} size="sm">
+            Report it
+          </Button>
+        </div>
+      );
+    }
+
+    return this.props.children;
+  }
+}
