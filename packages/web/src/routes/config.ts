c0fce6bdac770cbc5444f245feca6e18fb735cd0
Kashyap Gohil <kashyapgohil476@gmail.com>
2025-05-24 21:24:59 +0530

    [onboarding] add onboarding route to main routes config

--- packages/web/src/routes/config.ts
+++ packages/web/src/routes/config.ts
@@ -2,8 +2,10 @@ import { RouterConfig } from "@frameworks/router/type";
 import { HomeRoute } from "@home/route";
 import { ProjectsRoute } from "@projects/route";
 import { MailsRoute } from "src/modules/mails/route";
+import { OnboardingRoute } from "src/modules/onboarding/route";
 
 export const ROUTES_CONFIG: RouterConfig = [
+  OnboardingRoute,
   HomeRoute,
   MailsRoute,
   ProjectsRoute,
