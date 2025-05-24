d34fe23d6505f140b22d3513afb313fe3bfa3873
Kashyap Gohil <kashyapgohil476@gmail.com>
2025-05-24 22:01:51 +0530

    [route] setup auth router and protected routes through authenticate middleware

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -1,9 +1,29 @@
-import { Hono } from 'hono'
+import { auth, authenticate } from "@auth";
+import { mails } from "@mails";
+import "dotenv/config";
+import { Hono } from "hono";
+import { logger } from "hono/logger";
 
-const app = new Hono()
+const app = new Hono().basePath("/api");
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+// Global middleware
+app.use(logger());
 
-export default app
+// Public routes
+app.get("/", (c) => c.text("Howdy partner, how you doin?"));
+app.get("/ping", (c) =>
+  c.json<{ message: string }>({ message: "app is running" }),
+);
+
+// Auth routes
+app.route("/", auth);
+
+// Protected routes that require authentication
+const protectedRoutes = new Hono().basePath("/api");
+protectedRoutes.use("*", authenticate);
+
+protectedRoutes.route("/", mails);
+
+app.route("/", protectedRoutes);
+
+export default app;
