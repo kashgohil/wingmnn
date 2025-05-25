70e0f90def1727544a3f953025ff243f92043405
Kashyap Gohil <kashyapgohil476@gmail.com>
2025-05-25 15:44:51 +0530

    [polling] add args support to polling

--- packages/web/src/frameworks/query/polling.ts
+++ packages/web/src/frameworks/query/polling.ts
@@ -1,6 +1,6 @@
 export const Poll = {
-  poll: function (this: TSAny, fn: () => void, pollTime: number = 1000 * 60) {
-    const intervalId = setInterval(fn, pollTime);
-    return () => clearInterval(intervalId);
-  },
+	poll: function (this: TSAny, fn: (...args: TSAny[]) => void, pollTime: number = 1000 * 60, ...args: TSAny[]) {
+		const intervalId = setInterval(() => fn(...args), pollTime);
+		return () => clearInterval(intervalId);
+	},
 };
