cff08174d1e967dd614c9aa425082432757bff27
Kashyap Gohil <kashyapgohil476@gmail.com>
2025-05-25 15:45:08 +0530

    [polling] use MINUTE constant

--- packages/web/src/frameworks/query/polling.ts
+++ packages/web/src/frameworks/query/polling.ts
@@ -1,6 +1,13 @@
+import { MINUTE } from "@constants";
+
 export const Poll = {
-	poll: function (this: TSAny, fn: (...args: TSAny[]) => void, pollTime: number = 1000 * 60, ...args: TSAny[]) {
-		const intervalId = setInterval(() => fn(...args), pollTime);
-		return () => clearInterval(intervalId);
-	},
+  poll: function (
+    this: TSAny,
+    fn: (...args: TSAny[]) => void,
+    pollTime: number = MINUTE,
+    ...args: TSAny[]
+  ) {
+    const intervalId = setInterval(() => fn(...args), pollTime);
+    return () => clearInterval(intervalId);
+  },
 };
