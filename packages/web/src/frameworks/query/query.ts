e24a2f4a408be8664f522e8595b0d2df15feec55
Kashyap Gohil <kashyapgohil476@gmail.com>
2025-05-26 03:34:48 +0530

    [debug] classic this issue - moved functions to lambdas to leverage lexical scope for this

--- packages/web/src/frameworks/query/query.ts
+++ packages/web/src/frameworks/query/query.ts
@@ -7,13 +7,13 @@ import { Batch } from "./batching";
 import { Cache } from "./cache";
 import { Poll } from "./polling";
 
-interface QueryParams<T> {
+export interface QueryParams<T> {
   primaryKey: string;
-  secondaryKey: string;
-  params: T;
+  secondaryKey?: string;
+  params?: T;
 }
 
-interface Params<T, K, S> {
+export interface Params<T, K, S> {
   /**
    * The key to use for the query. ideally, this should be a unique identifier for the query. and this should hold all the dependencies for the query.
    */
@@ -86,7 +86,7 @@ export class Query<T, K, S = T> {
   refetching: boolean = false;
   status: "idle" | "fetching" | "error" | "success" = "idle";
 
-  #polling() {
+  #polling = () => {
     const key = this.#serialize(this.#params.key);
     if (
       this.#params.enabled &&
@@ -100,44 +100,20 @@ export class Query<T, K, S = T> {
       );
       this.#poll.set(key, clear);
     }
-  }
-
-  #init(params: Partial<Params<T, K, S>>, subscriber: () => void = noop) {
-    this.#params = merge(SANE_DEFAULT, params) as Params<T, K, S>;
-    this.#subscriber = subscriber;
-    this.#executor = batch.batch<T | null, QueryParams<K>>(this.#query);
-
-    if (params.enabled) {
-      this.#executor.call(null, this.#params.key);
-    }
-
-    this.#polling();
-  }
-
-  constructor(params: Params<T, K, S>, subscriber: () => void = noop) {
-    this.#init(params, subscriber);
-  }
-
-  updateParams(params: Params<T, K, S>) {
-    if (this.#serialize(this.#params.key) !== this.#serialize(params.key)) {
-      this.#init(params, this.#subscriber);
-    } else if (!!this.#params.enabled !== !!params.enabled) {
-      this.#init(params, this.#subscriber);
-    } else {
-      this.#params = params;
-    }
-  }
+  };
 
   #serialize(key: QueryParams<K>) {
     return serialize(key);
   }
 
-  async #query(key: QueryParams<K>) {
-    if (cache.has(key.primaryKey)) {
+  #query = async (key: QueryParams<K>) => {
+    if (cache.has(this.#serialize(key))) {
       this.#result = cache.get(this.#serialize(key)) as S;
     }
 
     this.status = "fetching";
+    if (this.status !== "fetching") this.#subscriber();
+
     const { result, error } = await tryCatchAsync(this.#params.queryFn, key);
 
     if (error) {
@@ -160,9 +136,35 @@ export class Query<T, K, S = T> {
     this.refetching = false;
 
     return result;
+  };
+
+  #init = (params: Partial<Params<T, K, S>>, subscriber: () => void = noop) => {
+    this.#params = merge(SANE_DEFAULT, params) as Params<T, K, S>;
+    this.#subscriber = subscriber;
+    this.#executor = batch.batch<T | null, QueryParams<K>>(this.#query);
+
+    if (this.#params.enabled) {
+      this.#executor.call(null, this.#params.key);
+    }
+
+    this.#polling();
+  };
+
+  constructor(params: Params<T, K, S>, subscriber: () => void = noop) {
+    this.#init(params, subscriber);
   }
 
-  async refetch() {
+  updateParams = (params: Params<T, K, S>) => {
+    if (this.#serialize(this.#params.key) !== this.#serialize(params.key)) {
+      this.#init(params, this.#subscriber);
+    } else if (!!this.#params.enabled !== !!params.enabled) {
+      this.#init(params, this.#subscriber);
+    } else {
+      this.#params = params;
+    }
+  };
+
+  refetch = async () => {
     this.refetching = true;
     this.status = "fetching";
     this.#subscriber();
@@ -191,7 +193,7 @@ export class Query<T, K, S = T> {
     this.#subscriber();
     this.initial = false;
     this.refetching = false;
-  }
+  };
 
   get error() {
     return this.#error;
