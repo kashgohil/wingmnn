4aa439d2e176a6aaf20c994718ee93ee1712f965
Kashyap Gohil <kashyapgohil476@gmail.com>
2025-05-26 01:53:13 +0530

    [auth] init router, export auth router from index file alongside all the routes

---
+++ packages/backend/src/auth/index.ts
@@ -0,0 +1,8 @@
+import { auth } from "@auth/router";
+import "@auth/routes/heartbeat";
+import "@auth/routes/login";
+import "@auth/routes/logout";
+import "@auth/routes/register";
+import "@auth/routes/sso.google";
+
+export { auth };
