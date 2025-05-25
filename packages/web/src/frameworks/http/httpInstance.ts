2fed8a05864e51e5328efdc506fddb5ca4365812
Kashyap Gohil <kashyapgohil476@gmail.com>
2025-05-26 01:25:06 +0530

    [http] export response and request interceptors

--- packages/web/src/frameworks/http/httpInstance.ts
+++ packages/web/src/frameworks/http/httpInstance.ts
@@ -2,37 +2,40 @@ import { RouterUtils } from "@frameworks/router/utils";
 import { Cookie } from "@utility/browser";
 import { httpService } from "./http";
 
+export function responseInterceptor(response: Response) {
+  // Handle response data or errors
+  if (response.status === 401) {
+    // Handle unauthorized error
+    Cookie.remove("csrf_token");
+    Cookie.remove("access_token");
+    Cookie.remove("refresh_token");
+    Cookie.remove("authenticated");
+    setTimeout(() => {
+      if (window.location.pathname === "") RouterUtils.reload();
+      RouterUtils.goTo("/");
+    }, 2000);
+  }
+  if (response.status === 403) {
+    // Handle forbidden error
+  }
+  if (response.status === 404) {
+    // Handle not found error
+  }
+  if (response.status >= 500) {
+    // Handle server error
+  }
+
+  return response.json();
+}
+
+export function requestInterceptor(request: Request) {
+  // Add custom headers or modify the request
+  request.headers.set("X-CSRF-Token", Cookie.get("csrf_token"));
+  return request;
+}
+
 export const http = httpService({
   baseUrl: "/api",
-  requestInterceptor: (request) => {
-    // Add custom headers or modify the request
-    request.headers.set("X-CSRF-Token", Cookie.get("csrf_token"));
-    return request;
-  },
-
-  responseInterceptor: (response) => {
-    // Handle response data or errors
-    if (response.status === 401) {
-      // Handle unauthorized error
-      Cookie.remove("csrf_token");
-      Cookie.remove("access_token");
-      Cookie.remove("refresh_token");
-      Cookie.remove("authenticated");
-      setTimeout(() => {
-        if (window.location.pathname === "") RouterUtils.reload();
-        RouterUtils.goTo("/");
-      }, 2000);
-    }
-    if (response.status === 403) {
-      // Handle forbidden error
-    }
-    if (response.status === 404) {
-      // Handle not found error
-    }
-    if (response.status >= 500) {
-      // Handle server error
-    }
-
-    return response.json();
-  },
+  requestInterceptor,
+  responseInterceptor,
 });
