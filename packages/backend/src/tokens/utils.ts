6ee777f631872ff5948378cea45735c4b19dcf54
Kashyap Gohil <kashyapgohil476@gmail.com>
2025-05-31 19:20:22 +0530

    [query] add tokens and users table query

---
+++ packages/backend/src/tokens/utils.ts
@@ -0,0 +1,23 @@
+import { db } from "@db";
+import { Key, Value } from "@db/constants";
+import { tokensTable, TokensTableType } from "@db/schema/tokens";
+import { eq } from "drizzle-orm";
+
+const query = db.query.tokensTable;
+
+export async function get<K extends Key<TokensTableType>>(
+  field: K,
+  value: Value<TokensTableType, K>,
+) {
+  return await query.findFirst({
+    where: eq(tokensTable[field], value),
+  });
+}
+
+export const tokensQuery = {
+  findFirst: query.findFirst,
+  findMany: query.findMany,
+  get,
+  insert: db.insert(tokensTable),
+  update: db.update(tokensTable),
+};
---
+++ packages/backend/src/users/utils.ts
@@ -0,0 +1,36 @@
+import { db } from "@db";
+import { Key, Value } from "@db/constants";
+import { User, usersTable, UsersTableType } from "@db/schema/users";
+import { eq } from "drizzle-orm";
+import { stripEmptyValues } from "utils";
+
+const query = db.query.usersTable;
+
+function sanitizeUser(user: User) {
+  return stripEmptyValues({
+    ...user,
+    password: null,
+    googleId: null,
+    authProvider: null,
+  });
+}
+
+export async function get<K extends Key<UsersTableType>>(
+  field: K,
+  value: Value<UsersTableType, K>,
+) {
+  const user = await query.findFirst({
+    where: eq(usersTable[field], value),
+  });
+
+  if (user) return sanitizeUser(user);
+  return null;
+}
+
+export const userQuery = {
+  findFirst: query.findFirst,
+  findMany: query.findMany,
+  get,
+  insert: db.insert(usersTable),
+  update: db.update(usersTable),
+};
