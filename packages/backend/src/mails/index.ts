7b3d0bf52c5f9a9ec5ef034fec9d6c01b4ec580e
Kashyap Gohil <kashyapgohil476@gmail.com>
2025-06-01 22:45:46 +0530

    [mails] cleanup

--- packages/backend/src/mails/index.ts
+++ packages/backend/src/mails/index.ts
@@ -1,14 +1,5 @@
-import { db } from "@db";
 import { Hono } from "hono";
 
 export const mails = new Hono().basePath("/mails");
 
-mails.get("/get/:id", async (c) => {
-  const { id } = c.req.param();
-  const mail = await db.mail.findUnique({ where: { id } });
-  if (!mail) return c.notFound();
-  return c.json(mail);
-});
-
-const promise =
-  Math.random() > 0.5 ? Promise.resolve("something") : Promise.reject("error");
+mails.get("/get/:id", async (c) => {});
