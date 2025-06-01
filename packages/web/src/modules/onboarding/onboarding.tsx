63556f979beb5cfe863d44fabba7dea2e5df6810
Kashyap Gohil <kashyapgohil476@gmail.com>
2025-06-01 22:43:31 +0530

    [onboarding] update styling

--- packages/web/src/modules/onboarding/onboarding.tsx
+++ packages/web/src/modules/onboarding/onboarding.tsx
@@ -8,22 +8,22 @@ export function Onboarding() {
   const [name, setName] = React.useState<string>("");
 
   return (
-    <div className="h-full w-full flex items-center justify-center">
+    <div className="h-full w-full flex items-center justify-center relative">
+      <ConfettiAnimation className="bg-black-300 z-0" />
       <Card
         size="lg"
-        className="bg-black-300 flex flex-col items-center justify-center p-20 gap-8 relative overflow-hidden"
+        className="flex flex-col items-center justify-center p-20 gap-8 relative overflow-hidden bg-black-100"
         animate={{ opacity: 1, translateY: 0 }}
         initial={{ opacity: 0.2, translateY: 20 }}
       >
-        <ConfettiAnimation className="bg-black-300" />
-        <Typography.H1 className="text-center z-1 w-fit">Hurray!</Typography.H1>
-        <Typography.H2 className="text-center z-1 w-fit">
+        <Typography.H1 className="text-center w-fit">Hurray!</Typography.H1>
+        <Typography.H2 className="text-center w-fit">
           You are most welcome, Kash.
         </Typography.H2>
-        <Typography.Paragraph className="text-center z-1 w-fit">
+        <Typography.Paragraph className="text-center w-fit">
           it's me, your wingmnn!!
         </Typography.Paragraph>
-        <Typography.Paragraph className="text-center z-1 w-fit">
+        <Typography.Paragraph className="text-center w-fit">
           I know it's weird to call someone who doesn't have any name, right?
           let's decide what you would like to call me:
         </Typography.Paragraph>
@@ -33,7 +33,7 @@ export function Onboarding() {
           variant="outlined"
           onChange={setName}
           wrapperClassName="z-1"
-          className="text-center text-2xl z-1 w-fit"
+          className="text-center text-2xl w-fit"
         />
       </Card>
     </div>
