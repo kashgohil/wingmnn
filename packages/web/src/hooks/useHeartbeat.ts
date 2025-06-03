21fa485c3bf8ceca6988dba4bfc336cfeadf52b5
Kashyap Gohil <kashyapgohil476@gmail.com>
2025-06-03 09:58:25 +0530

    [chore] constant query key for heartbeat

--- packages/web/src/hooks/useHeartbeat.ts
+++ packages/web/src/hooks/useHeartbeat.ts
@@ -1,11 +1,12 @@
 import { MINUTE } from "@constants";
 import { QUICK_STALE } from "@frameworks/query/constants";
 import { useQuery } from "@frameworks/query/hook";
+import { HEARTBEAT_QUERY_KEY } from "@queryKeys";
 import { AuthService } from "@services/authService";
 
 export function useHeartbeat() {
   useQuery({
-    key: { primaryKey: "HEARTBEAT" },
+    key: HEARTBEAT_QUERY_KEY,
     queryFn: AuthService.heartbeat,
     staleTime: QUICK_STALE,
     polling: {
--- packages/web/src/queryKeys.ts
+++ packages/web/src/queryKeys.ts
@@ -1,3 +1,2 @@
-export const ME_QUERY_KEY = {
-  primaryKey: "ME",
-};
+export const HEARTBEAT_QUERY_KEY = { primaryKey: "HEARTBEAT" };
+export const ME_QUERY_KEY = { primaryKey: "ME" };
