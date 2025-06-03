a7dac4c2986fe557305f4e7ad01a1750913bf21b
Kashyap Gohil <kashyapgohil476@gmail.com>
2025-06-03 19:51:06 +0530

    [heartbeat] update heartbeat loading state to maintain the loading page

--- packages/web/src/hooks/useHeartbeat.ts
+++ packages/web/src/hooks/useHeartbeat.ts
@@ -1,11 +1,19 @@
-import { MINUTE } from "@constants";
+import { MINUTE, SECOND } from "@constants";
 import { QUICK_STALE } from "@frameworks/query/constants";
 import { useQuery } from "@frameworks/query/hook";
 import { HEARTBEAT_QUERY_KEY } from "@queryKeys";
 import { AuthService } from "@services/authService";
+import { useBoolean } from "@wingmnn/utils/hooks";
+import React from "react";
 
 export function useHeartbeat() {
-  useQuery({
+  const {
+    value: loading,
+    unset: unsetLoading,
+    set: setLoading,
+  } = useBoolean(false);
+
+  const { isLoading, isRefetching } = useQuery({
     key: HEARTBEAT_QUERY_KEY,
     queryFn: AuthService.heartbeat,
     staleTime: QUICK_STALE,
@@ -14,4 +22,20 @@ export function useHeartbeat() {
       interval: 4 * MINUTE,
     },
   });
+
+  React.useEffect(() => {
+    let timeoutId: NodeJS.Timeout;
+    if (isLoading && !isRefetching) {
+      setLoading();
+      timeoutId = setTimeout(() => {
+        unsetLoading();
+      }, 4 * SECOND);
+
+      return () => timeoutId && clearTimeout(timeoutId);
+    }
+
+    // return () => timeoutId && clearTimeout(timeoutId);
+  }, [unsetLoading, setLoading, isLoading, isRefetching]);
+
+  return { isLoading: loading || (isLoading && !isRefetching) };
 }
