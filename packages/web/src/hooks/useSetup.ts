8a6ff7e3c343f67f46b4290a489d28cc5e097031
Kashyap Gohil <kashyapgohil476@gmail.com>
2025-06-03 09:58:43 +0530

    [chore] update stale time for setup

--- packages/web/src/hooks/useSetup.ts
+++ packages/web/src/hooks/useSetup.ts
@@ -1,12 +1,14 @@
-import { INFINITE } from "@frameworks/query/constants";
+import { LONG_STALE } from "@frameworks/query/constants";
 import { useQuery } from "@frameworks/query/hook";
 import { SetupService } from "@services/setupService";
+import type { User } from "@wingmnn/db";
 import { ME_QUERY_KEY } from "src/queryKeys";
 
 export function useSetup() {
-  useQuery({
+  useQuery<TSAny, TSAny, User>({
     queryFn: SetupService.me,
     key: ME_QUERY_KEY,
-    staleTime: INFINITE,
+    staleTime: LONG_STALE,
+    selector: (response) => response.user,
   });
 }
