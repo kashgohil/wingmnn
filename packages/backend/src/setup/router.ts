adad1cdfb5bc229b16f163b3035ef0b2dc65083b
Kashyap Gohil <kashyapgohil476@gmail.com>
2025-06-06 15:33:41 +0530

    [fix] update type import

--- packages/backend/src/setup/router.ts
+++ packages/backend/src/setup/router.ts
@@ -1,4 +1,4 @@
-import { AuthenticateEnv } from "@auth/middleware";
+import { AuthenticateEnv } from "@types";
 import { Hono } from "hono";
 
 export const setup = new Hono<AuthenticateEnv>().basePath("/setup");
