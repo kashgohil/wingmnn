c028ba821d808256ee7c2c71a4bf529c95e2cf63
Kashyap Gohil <kashyapgohil476@gmail.com>
2025-06-07 19:16:25 +0530

    [tasks] add more columns to TasksTable

--- packages/db/src/schema/tasks.ts
+++ packages/db/src/schema/tasks.ts
@@ -1,9 +1,28 @@
-import { pgTable, text, varchar } from "drizzle-orm/pg-core";
+import { pgEnum, pgTable, text, varchar } from "drizzle-orm/pg-core";
 import { commonFields } from "../constants";
+import { usersTable } from "./users";
+
+export const TaskTypeEnum = pgEnum("task_type", [
+  "feature",
+  "enhancement",
+  "sub_task",
+  "task",
+  "bug",
+  "story",
+]);
 
 export const tasksTable = pgTable("tasks", {
-  name: varchar("name", { length: 255 }).notNull(),
+  key: varchar("key", { length: 255 }).notNull(),
+  title: varchar("name", { length: 255 }).notNull(),
   content: text("content").notNull(),
+  type: TaskTypeEnum("type").notNull().default("task"),
+  assignedTo: varchar("assigned_to", { length: 255 }).references(
+    () => usersTable.id,
+    {
+      onDelete: "no action",
+    },
+  ),
+  status: varchar("status", { length: 255 }).notNull(),
   ...commonFields,
 });
 
