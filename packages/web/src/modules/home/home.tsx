3756c940ac0161d9f840ffe53dcf300277d90b54
Kashyap Gohil <kashyapgohil476@gmail.com>
2025-06-23 00:42:56 +0400

    [chore] user

--- packages/web/src/modules/home/home.tsx
+++ packages/web/src/modules/home/home.tsx
@@ -1,7 +1,13 @@
+import { useQueryState } from "@frameworks/query/hook";
+import { ME_QUERY_KEY } from "@queryKeys";
+import type { User } from "@wingmnn/db";
+
 export function Home() {
+  const user = useQueryState<User>(ME_QUERY_KEY);
+
   return (
     <div className="flex flex-col h-full items-center justify-center">
-      <h1 className="text-4xl font-light">Welcome Home, Kash!</h1>
+      <h1 className="text-4xl font-light">Welcome, {user?.name}!</h1>
     </div>
   );
 }
