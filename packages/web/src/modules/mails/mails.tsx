a88ed461dc5eb30e04612b055e2fda0c248e31be
Kashyap Gohil <kashyapgohil476@gmail.com>
2025-06-27 22:36:54 +0530

    [chore] remove the fluff

--- packages/web/src/modules/mails/mails.tsx
+++ packages/web/src/modules/mails/mails.tsx
@@ -1,9 +1,3 @@
-import {
-  ResizableHandle,
-  ResizablePanel,
-  ResizablePanelGroup,
-} from "@wingmnn/components";
-
 interface Props {
   children: React.ReactNode;
 }
@@ -11,19 +5,5 @@ interface Props {
 export function Mails(props: Props) {
   const { children } = props;
 
-  return (
-    <div className="flex h-full w-full">
-      <ResizablePanelGroup direction="horizontal" autoSaveId={"MAIL_SPLIT"}>
-        <ResizablePanel className="bg-black-200" defaultValue={"20%"}>
-          <div className="flex flex-col p-3 h-full"></div>
-        </ResizablePanel>
-        <ResizableHandle
-          withHandle
-          handleClassName="text-gray-500"
-          className="border-l border-l-black-100 border-r border-r-black-200 hover:border-black-200 active:border-black-200 transition-colors duration-100"
-        />
-        <ResizablePanel defaultValue={"80%"}>{children}</ResizablePanel>
-      </ResizablePanelGroup>
-    </div>
-  );
+  return <div className="flex h-full w-full">{children}</div>;
 }
