22a6c6ee58ff070394ab5e986832d91c84a5f829
Kashyap Gohil <kashyapgohil476@gmail.com>
2025-06-29 15:37:12 +0530

    [chore] clean up

--- packages/utils/src/interactivity/escape.ts
+++ packages/utils/src/interactivity/escape.ts
@@ -2,7 +2,6 @@ import { type KeyboardEvent } from "react";
 
 export function escape(fn: (e: KeyboardEvent<HTMLElement>) => void) {
   return (e: KeyboardEvent<HTMLElement>) => {
-    console.log(e);
     switch (e.code) {
       case "Escape":
         fn(e);
