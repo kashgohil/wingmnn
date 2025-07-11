da7fc2e61b9a0530ac6b34029ee8306f1a13fb2d
Kashyap Gohil <kashyapgohil476@gmail.com>
2025-07-11 13:25:48 +0530

    [chore] views

--- packages/web/src/constants.ts
+++ packages/web/src/constants.ts
@@ -3,3 +3,11 @@ export const spacing = 4;
 export const SECOND = 1000;
 export const MINUTE = 60 * SECOND;
 export const HOUR = 60 * MINUTE;
+
+export enum Views {
+  GRID = "GRID",
+  LIST = "LIST",
+  CALENDAR = "CALENDAR",
+  TIMELINE = "TIMELINE",
+  CHART = "CHART",
+}
