17d5ad2686090e4f1671e2ee8ba9d8752bf4c813
Kashyap Gohil <kashyapgohil476@gmail.com>
2025-07-15 22:37:57 +0530

    [chore] router context setup

---
+++ packages/router/src/context.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import type { RouterConfig } from "./type";
+import { RouterUtils } from "./utils";
+
+interface RouterContextProps {
+  config: RouterConfig;
+}
+
+export const RouterContext = React.createContext<RouterContextProps>({
+  config: [],
+});
+
+export const RouterProvider = ({
+  config,
+  children,
+}: {
+  config: RouterConfig;
+  children: React.ReactNode;
+}) => {
+  const processedRoutes = React.useMemo(() => {
+    return RouterUtils.processRoutes(config);
+  }, [config]);
+
+  return (
+    <RouterContext.Provider value={{ config: processedRoutes }}>
+      {children}
+    </RouterContext.Provider>
+  );
+};
+
+export function useRouterContext(key: keyof RouterContextProps) {
+  const { [key]: value } = React.useContext(RouterContext);
+  return value;
+}
