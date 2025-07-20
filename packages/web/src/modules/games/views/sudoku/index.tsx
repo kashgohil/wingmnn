81fb302fcea367d1dff6177f04582c09bf371e7d
Kashyap Gohil <kashyapgohil476@gmail.com>
2025-07-20 12:30:56 +0530

    [chore] add sudoku game view

--- packages/web/src/modules/games/views/sudoku/index.tsx
+++ packages/web/src/modules/games/views/sudoku/index.tsx
@@ -1,9 +1,21 @@
-import { useQuery } from "@frameworks/query/hook";
+import { useMutation, useQuery } from "@frameworks/query/hook";
 import { Games } from "@games/constants";
 import { SudokuService } from "@games/services/sudokuService";
-import { Button, Tabs, Typography } from "@wingmnn/components";
+import { Wingmnn } from "@icons/wingmnn";
+import {
+  Button,
+  Input,
+  Tabs,
+  Typography,
+  useToast,
+  type Tab,
+} from "@wingmnn/components";
+import type { Sudoku } from "@wingmnn/db";
+import { RouterUtils } from "@wingmnn/router";
+import type { CreateSudokuPayload, Difficulty } from "@wingmnn/types";
 import React from "react";
 import { GameError } from "../error";
+import { GameLoading } from "../loading";
 
 interface SudokuProps {
   gameId: string;
@@ -19,7 +31,7 @@ export function Sudoku(props: SudokuProps) {
     [gameId],
   );
 
-  const { error, result: game } = useQuery({
+  const { error, isLoading } = useQuery({
     key: queryKey,
     queryFn: () => {
       return SudokuService.get(gameId);
@@ -31,6 +43,10 @@ export function Sudoku(props: SudokuProps) {
     return <GameError game={Games.SUDOKU} />;
   }
 
+  if (isLoading) {
+    return <GameLoading game={Games.SUDOKU} />;
+  }
+
   if (!gameId) {
     return <Create />;
   }
@@ -38,27 +54,81 @@ export function Sudoku(props: SudokuProps) {
   return <div className="p-4 h-full w-full"></div>;
 }
 
-const DIFFICULTIES = [
+const DIFFICULTIES: Array<Tab<Difficulty>> = [
   { id: "easy", name: "Easy", description: "" },
   { id: "medium", name: "Medium", description: "" },
   { id: "hard", name: "Hard", description: "" },
 ];
 
 function Create() {
-  const [difficulty, setDifficulty] = React.useState<string>("easy");
+  const [size, setSize] = React.useState<number>(9);
+  const [difficulty, setDifficulty] = React.useState<Difficulty>("easy");
+
+  const toast = useToast();
+
+  const queryKey = React.useMemo(
+    () => ({ primaryKey: "SUDOKU", params: { size, difficulty } }),
+    [size, difficulty],
+  );
+
+  const { isMutating, mutate: create } = useMutation({
+    key: queryKey,
+    staleTime: 0,
+    mutationFn: () => {
+      return SudokuService.create<Sudoku, CreateSudokuPayload>({
+        size,
+        difficulty,
+      });
+    },
+    onResolve: (res) => {
+      toast({
+        type: "success",
+        title: "Success",
+        description: "Game created!",
+      });
+      RouterUtils.goTo(`/games/sudoku/${res.data.id}`);
+    },
+    onReject(error) {
+      toast({
+        type: "error",
+        title: "Uh Oh!!",
+        description: error.message || "Something went wrong",
+      });
+    },
+  });
 
   return (
-    <div className="h-full w-full flex flex-col items-center justify-center gap-4">
+    <div className="h-2/3 w-full flex flex-col items-center justify-center gap-4">
       <Typography.H1 className="font-spicy-rice text-accent">
         How brave are you?
       </Typography.H1>
-      <Tabs
+      <Tabs<Difficulty>
+        tabs={DIFFICULTIES}
+        tabClassName="text-sm"
         activeTab={difficulty}
         onChange={setDifficulty}
-        tabClassName="text-sm"
-        tabs={DIFFICULTIES}
       />
-      <Button size="sm">Let's Play</Button>
+      <Typography.H1 className="font-spicy-rice text-accent">
+        What grid size you wanna try?
+      </Typography.H1>
+      <Input
+        min={1}
+        max={9}
+        value={size}
+        type="number"
+        onChange={setSize}
+        variant="outlined"
+        placeholder="Grid Size"
+        className="text-center appearance-none"
+      />
+      <Button
+        onClick={create}
+        disabled={isMutating}
+        className="flex items-center gap-2"
+      >
+        {isMutating && <Wingmnn size={12} className="animate-spin" />}
+        Let's Play
+      </Button>
     </div>
   );
 }
