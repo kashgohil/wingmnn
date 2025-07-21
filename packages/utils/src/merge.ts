327a4b1dabb6af89ed6b5e653bb704dd8b70080e
Kashyap Gohil <kashyapgohil476@gmail.com>
2025-07-22 01:12:35 +0530

    [chore] update merge return type

--- packages/utils/src/merge.ts
+++ packages/utils/src/merge.ts
@@ -1,23 +1,23 @@
-import { forEachObj } from './forEach';
+import { forEachObj } from "./forEach";
 
-export function merge<T>(...objects: MapOf<TSAny>[]): T | undefined {
-	if (objects.length === 0) {
-		return undefined;
-	}
+export function merge<T>(...objects: MapOf<TSAny>[]): T {
+  if (objects.length === 0) {
+    return {} as T;
+  }
 
-	if (objects.length === 1) {
-		return objects[0] as T;
-	}
+  if (objects.length === 1) {
+    return objects[0] as T;
+  }
 
-	// do a deep merge here
-	return objects.reduce((acc, obj) => {
-		forEachObj(obj, (value, key) => {
-			if (typeof value === 'object') {
-				acc[key] = merge(acc[key], value);
-			} else {
-				acc[key] = value;
-			}
-		});
-		return acc;
-	}, {} as MapOf<TSAny>) as T;
+  // do a deep merge here
+  return objects.reduce((acc, obj) => {
+    forEachObj(obj, (value, key) => {
+      if (typeof value === "object") {
+        acc[key] = merge(acc[key], value);
+      } else {
+        acc[key] = value;
+      }
+    });
+    return acc;
+  }, {} as MapOf<TSAny>) as T;
 }
