64f1374aebf24fdc34b2a48ab00d5f48f8414a6e
Kashyap Gohil <kashyapgohil476@gmail.com>
2025-09-26 19:35:52 +0530

    [fix] style changes

--- packages/web/src/modules/navigation/navigation.tsx
+++ packages/web/src/modules/navigation/navigation.tsx
@@ -49,7 +49,7 @@ export function Navigation(props: { activeModule: Modules }) {
         <Link
           to={BaseRoutes[Modules.HOME]}
           onClick={playClickSound}
-          className="focus-within:outline-2 focus-within:outline-offset-6 focus-within:outline-accent transition-all duration-100 focus-within:rounded-lg"
+          className="focus-within:outline-2 focus-within:outline-offset-6 focus-within:outline-accent transition-all duration-100 focus-within:rounded"
         >
           <Wingmnn
             width={24}
