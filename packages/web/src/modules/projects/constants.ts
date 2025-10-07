bf579b30a54957eb874ef861ab710bc57a39e3b6
Kashyap Gohil <kashyapgohil476@gmail.com>
2025-10-08 01:31:21 +0530

    [cleanup] remove status enum

--- packages/web/src/modules/projects/constants.ts
+++ packages/web/src/modules/projects/constants.ts
@@ -1,9 +1,3 @@
-export enum Status {
-  TODO = "TODO",
-  IN_PROGRESS = "IN_PROGRESS",
-  DONE = "DONE",
-}
-
 export enum ProjectDialog {
   CREATE_PROJECT = "CREATE_PROJECT",
   EDIT_PROJECT = "EDIT_PROJECT",
