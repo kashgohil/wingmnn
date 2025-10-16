6f6bde5fa0234779939ee72fa02263807b36cf4f
Kashyap Gohil <kashyapgohil476@gmail.com>
2025-10-17 04:10:12 +0530

    [projects] updated web project service

--- packages/web/src/modules/projects/services/projectsService.ts
+++ packages/web/src/modules/projects/services/projectsService.ts
@@ -1,18 +1,32 @@
 import { http } from "@frameworks/http/httpInstance";
+import type { Workflow, WorkflowStatus } from "@wingmnn/db";
+import type { ResponseWrapper } from "@wingmnn/types";
 import { type Project } from "../type";
 
 export const ProjectsService = (function () {
   async function getProjects() {
-    return await http.get<Array<Project>>("/api/projects");
+    return await http.get<Array<Project>>("/projects");
   }
   async function createProject(project: Project) {
-    return await http.put<Project>("/api/projects", project);
+    return await http.put<Project>("/projects", project);
   }
   async function deleteProject(projectId: string) {
-    return await http.delete<Project>(`/api/projects/${projectId}`);
+    return await http.delete<Project>(`/projects/${projectId}`);
   }
   async function updateProject(projectId: string, project: Partial<Project>) {
-    return await http.put<Project>(`/api/projects/${projectId}`, project);
+    return await http.put<Project>(`/projects/${projectId}`, project);
+  }
+
+  async function getWorkflows() {
+    return await http.get<ResponseWrapper<Array<Workflow>>>(
+      "/projects/workflows",
+    );
+  }
+
+  async function getStatusForWorkflows(workflowIds: Array<string>) {
+    return await http.post<
+      ResponseWrapper<Record<string, Array<WorkflowStatus>>>
+    >("/projects/workflows/multiple-workflow-statuses", { workflowIds });
   }
 
   return {
@@ -20,5 +34,8 @@ export const ProjectsService = (function () {
     createProject,
     deleteProject,
     updateProject,
+
+    getWorkflows,
+    getStatusForWorkflows,
   };
 })();
