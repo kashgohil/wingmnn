240cf4bdfe80e7d87b73bdce3e39eb92fead743f
Kashyap Gohil <kashyapgohil476@gmail.com>
2025-10-16 22:56:13 +0530

    [chore] type for setup service

--- packages/web/src/services/setupService.ts
+++ packages/web/src/services/setupService.ts
@@ -1,8 +1,10 @@
 import { http } from "@frameworks/http/httpInstance";
+import type { User } from "@wingmnn/db";
+import type { ResponseWrapper } from "@wingmnn/types";
 
 export const SetupService = (function () {
   function me() {
-    return http.get("/setup/me");
+    return http.get<ResponseWrapper<User>>("/setup/me");
   }
   return {
     me,
