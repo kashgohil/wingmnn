9805699e8a00643a5c63c25d6626e5284a056c00
Kashyap Gohil <kashyapgohil476@gmail.com>
2025-10-19 01:20:14 +0530

    [projects] added query keys for projects and project workflows fetch

--- packages/web/src/queryKeys.ts
+++ packages/web/src/queryKeys.ts
@@ -1,5 +1,6 @@
 export const HEARTBEAT_QUERY_KEY = { primaryKey: "HEARTBEAT" };
 export const ME_QUERY_KEY = { primaryKey: "ME" };
-export const PROJECT_WORKFLOW_KEY = { primaryKey: "PROJECT_WORKFLOW" };
 
+export const PROJECTS_KEY = { primaryKey: "PROJECTS" };
+export const PROJECT_WORKFLOW_KEY = { primaryKey: "PROJECT_WORKFLOW" };
 export const WORKFLOW_STATUS_PRIMARY_KEY = "WORKFLOW_STATUS";
