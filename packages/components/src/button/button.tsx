890a2070905bfc64c1a3f60968344eafee04dcb8
Kashyap Gohil <kashyapgohil476@gmail.com>
2025-10-22 13:21:35 +0530

    [components][button] changed icon button and button props handling

--- packages/components/src/button/button.tsx
+++ packages/components/src/button/button.tsx
@@ -3,49 +3,46 @@ import { cx } from "@utility/cx";
 import { withClickSound } from "@wingmnn/utils/interactivity";
 
 export interface ButtonProps
-  extends React.DetailedHTMLProps<
-    React.ButtonHTMLAttributes<HTMLButtonElement>,
-    HTMLButtonElement
-  > {
-  variant?: "primary" | "secondary" | "icon" | "stripped";
-  size?: "sm" | "md" | "lg";
-  noSound?: boolean;
+	extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+	variant?: "primary" | "secondary" | "icon" | "stripped";
+	size?: "sm" | "md" | "lg";
+	noSound?: boolean;
 }
 
 const variantClasses = classVariance({
-  stripped: "!p-0",
-  icon: "bg-transparent text-accent hover:bg-accent/10 focus-within:outline-accent",
-  primary:
-    "bg-accent text-[var(--accent-text)] hover:bg-accent/70 focus-within:outline-accent",
-  secondary:
-    "bg-transparent border border-accent/40 text-accent hover:bg-accent/10 focus-within:outline-accent",
-  sm: "px-4 py-2",
-  md: "px-8 py-4",
-  lg: "px-12 py-8",
+	stripped: "!p-0",
+	icon: "bg-transparent text-accent hover:bg-accent/10 focus-within:outline-accent",
+	primary:
+		"bg-accent text-[var(--accent-text)] hover:bg-accent/70 focus-within:outline-accent",
+	secondary:
+		"bg-transparent border border-accent/40 text-accent hover:bg-accent/10 focus-within:outline-accent",
+	sm: "px-4 py-2",
+	md: "px-8 py-4",
+	lg: "px-12 py-8",
 });
 
 export function Button(props: ButtonProps) {
-  const {
-    children,
-    className,
-    size = "sm",
-    onClick,
-    variant = "primary",
-    noSound = false,
-    ...rest
-  } = props;
+	const {
+		children,
+		className,
+		size = "sm",
+		onClick,
+		variant = "primary",
+		noSound = false,
+		...rest
+	} = props;
 
-  return (
-    <button
-      {...rest}
-      onClick={noSound ? onClick : withClickSound(onClick)}
-      className={cx(
-        "rounded-lg active:translate-y-0.5 transition-all cursor-pointer focus-within:outline-2 outline-offset-2",
-        variantClasses(variant, size),
-        className,
-      )}
-    >
-      {children}
-    </button>
-  );
+	return (
+		<button
+			{...rest}
+			onClick={noSound ? onClick : withClickSound(onClick)}
+			className={cx(
+				"rounded-lg active:translate-y-0.5 transition-all cursor-pointer focus-within:outline-2 outline-offset-2",
+				variantClasses(variant, size),
+				className
+			)}
+		>
+			{children}
+		</button>
+	);
 }
--- packages/components/src/iconButton/iconButton.tsx
+++ packages/components/src/iconButton/iconButton.tsx
@@ -1,64 +1,69 @@
-import { Button, type ButtonProps } from "@components/button/button";
+import { Button } from "@components/button/button";
 import { cx } from "@utility/cx";
 import { type LucideProps } from "lucide-react";
+import type { ButtonHTMLAttributes } from "react";
 
-export interface IconButtonProps extends ButtonProps {
-  icon: React.JSXElementConstructor<LucideProps>;
-  shape?: "circle" | "rounded" | "square";
-  iconProps?: LucideProps;
-  className?: string;
+export interface IconButtonProps
+	extends ButtonHTMLAttributes<HTMLButtonElement> {
+	icon: React.JSXElementConstructor<LucideProps>;
+	shape?: "circle" | "rounded" | "square";
+	iconProps?: LucideProps;
+	className?: string;
+	variant?: "primary" | "secondary" | "icon" | "stripped";
+	size?: "sm" | "md" | "lg";
+	noSound?: boolean;
 }
 
 export function IconButton(props: IconButtonProps) {
-  const {
-    icon: Icon,
-    shape = "rounded",
-    iconProps = {},
-    className,
-    ...rest
-  } = props;
+	const {
+		icon: Icon,
+		shape = "rounded",
+		iconProps = {},
+		className,
+		...rest
+	} = props;
 
-  if (!Icon) return null;
+	if (!Icon) return null;
 
-  if (shape === "circle") {
-    return (
-      <Button
-        {...rest}
-        variant="icon"
-        className={cx(
-          "flex items-center justify-center rounded-full",
-          className,
-        )}
-      >
-        <Icon {...iconProps} />
-      </Button>
-    );
-  }
+	if (shape === "circle") {
+		return (
+			<Button
+				{...rest}
+				variant="icon"
+				className={cx(
+					"flex items-center justify-center rounded-full",
+					className
+				)}
+			>
+				<Icon {...iconProps} />
+			</Button>
+		);
+	}
 
-  if (shape === "rounded") {
-    return (
-      <Button
-        {...rest}
-        variant="icon"
-        className={cx("flex items-center justify-center rounded-lg", className)}
-      >
-        <Icon {...iconProps} />
-      </Button>
-    );
-  }
+	if (shape === "rounded") {
+		return (
+			<Button
+				{...rest}
+				variant="icon"
+				className={cx("flex items-center justify-center rounded-lg", className)}
+			>
+				<Icon {...iconProps} />
+			</Button>
+		);
+	}
 
-  if (shape === "square") {
-    return (
-      <Button
-        {...rest}
-        variant="icon"
-        className={cx(
-          "flex items-center justify-center rounded-none",
-          className,
-        )}
-      >
-        <Icon {...iconProps} />
-      </Button>
-    );
-  }
+	if (shape === "square") {
+		return (
+			<Button
+				{...rest}
+				variant="icon"
+				className={cx(
+					"flex items-center justify-center rounded-none",
+					className
+				)}
+			>
+				<Icon {...iconProps} />
+			</Button>
+		);
+	}
 }
