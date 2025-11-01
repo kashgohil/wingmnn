0ab67fe0f00cae21ea014967e4acc4335d2d672b
Kashyap Gohil <kashyapgohil476@gmail.com>
2025-11-01 15:31:24 +0530

    [users] type update

--- packages/web/src/services/userService.ts
+++ packages/web/src/services/userService.ts
@@ -1,22 +1,27 @@
 import { http } from "@frameworks/http/httpInstance";
 import type { NewUser, User } from "@wingmnn/db";
+import type { ResponseWrapper } from "@wingmnn/types";
 
 export const UserService = (function () {
   function update(userID: string, updates: Partial<Omit<User, "id">>) {
-    return http.put<User>(`/users/update/${userID}`, updates);
+    return http.put<ResponseWrapper<User>>(`/users/update/${userID}`, updates);
   }
 
   function del(userID: string) {
-    return http.delete<User>(`/users/delete/${userID}`);
+    return http.delete<ResponseWrapper<User>>(`/users/delete/${userID}`);
   }
 
   function create(user: NewUser) {
-    return http.post<User>("/users/create", user);
+    return http.post<ResponseWrapper<User>>("/users/create", user);
   }
 
   function get(userID: string) {
-    return http.get<User>(`/users/get/${userID}`);
+    return http.get<ResponseWrapper<User>>(`/users/get/${userID}`);
   }
 
-  return { update, delete: del, create, get };
+  function lookup(userIds: Array<string>) {
+    return http.post<ResponseWrapper<Array<User>>>("/users/lookup", userIds);
+  }
+
+  return { update, delete: del, create, get, lookup };
 })();
