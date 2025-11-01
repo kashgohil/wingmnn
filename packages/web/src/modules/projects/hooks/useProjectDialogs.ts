73cf5f8967ba7ed66a7e963ce5ae6fd923565e93
Kashyap Gohil <kashyapgohil476@gmail.com>
2025-11-01 16:08:43 +0530

    [projects] added extra dialogs for project

--- packages/web/src/modules/projects/hooks/useProjectDialogs.ts
+++ packages/web/src/modules/projects/hooks/useProjectDialogs.ts
@@ -2,44 +2,45 @@ import { createStore } from "@frameworks/store/create";
 import { ProjectDialog } from "@projects/constants";
 
 interface ProjectsState {
-	dialogs: Record<ProjectDialog, { open: boolean; params?: TSAny }>;
+  dialogs: Record<ProjectDialog, { open: boolean; params?: TSAny }>;
 }
 
 const { useState: useProjects, store } = createStore<ProjectsState>({
-	dialogs: {
-		[ProjectDialog.CREATE_PROJECT]: { open: false },
-		[ProjectDialog.EDIT_PROJECT]: { open: false },
-		[ProjectDialog.CREATE_TASK]: { open: false },
-		[ProjectDialog.EDIT_TASK]: { open: false },
-	},
+  dialogs: {
+    [ProjectDialog.CREATE_PROJECT]: { open: false },
+    [ProjectDialog.EDIT_PROJECT]: { open: false },
+    [ProjectDialog.CREATE_TASK]: { open: false },
+    [ProjectDialog.EDIT_TASK]: { open: false },
+    [ProjectDialog.PROJECT_SETTINGS]: { open: false },
+  },
 });
 
 const ProjectActions = (function () {
-	return {
-		openDialog: <T>(dialog: ProjectDialog, params?: T) => {
-			store.set("dialogs", {
-				...store.get("dialogs"),
-				[dialog]: { open: true, params },
-			});
-		},
+  return {
+    openDialog: <T>(dialog: ProjectDialog, params?: T) => {
+      store.set("dialogs", {
+        ...store.get("dialogs"),
+        [dialog]: { open: true, params },
+      });
+    },
 
-		closeDialog: (dialog: ProjectDialog) => {
-			store.set("dialogs", {
-				...store.get("dialogs"),
-				[dialog]: { open: false },
-			});
-		},
+    closeDialog: (dialog: ProjectDialog) => {
+      store.set("dialogs", {
+        ...store.get("dialogs"),
+        [dialog]: { open: false },
+      });
+    },
 
-		toggleDialog: (dialog: ProjectDialog) => {
-			store.set("dialogs", {
-				...store.get("dialogs"),
-				[dialog]: {
-					open: !store.get("dialogs")[dialog].open,
-					params: store.get("dialogs")[dialog].params,
-				},
-			});
-		},
-	};
+    toggleDialog: (dialog: ProjectDialog) => {
+      store.set("dialogs", {
+        ...store.get("dialogs"),
+        [dialog]: {
+          open: !store.get("dialogs")[dialog].open,
+          params: store.get("dialogs")[dialog].params,
+        },
+      });
+    },
+  };
 })();
 
 export { ProjectActions, useProjects };
